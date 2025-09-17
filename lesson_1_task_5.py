381b4ae83cf432da8408eecabb7aa5b68b0cdab5
Eugenia0331 <shelkovnikova.eugenia@gmail.com>
2025-09-17 20:15:37 +0300

    Домашка

---
+++ lesson_1_task_5.py
@@ -0,0 +1,14 @@
+def print_nubmer(num):
+    print(num, end='')
+
+print_nubmer('8')
+print_nubmer('8')
+print_nubmer('0')
+print_nubmer('0')
+print_nubmer('5')
+print_nubmer('5')
+print_nubmer('5')
+print_nubmer('3')
+print_nubmer('5')
+print_nubmer('3')
+print_nubmer('5')
